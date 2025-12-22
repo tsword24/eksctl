d4d96c292f59f4a02637e71d6d41ec02a1c9d5ef
khajit <ssvvkhajit@gmail.com>
2025-12-22 15:17:41 +0530

    install eksctl and kubectl

--- workstation.sh
+++ workstation.sh
@@ -18,3 +18,5 @@ PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+aws configure
