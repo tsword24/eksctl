2d6226aeb276c357c58191203a7965da3d686e42
khajit <ssvvkhajit@gmail.com>
2025-12-22 15:27:48 +0530

    install eksctl and kubectl

--- workstation.sh
+++ workstation.sh
@@ -9,6 +9,7 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 
+cd ..
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
